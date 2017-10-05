b41a4de73c5083f9e93c9a91850b388b3b37d640
Christian Bradley <christian.bradley@bocodigital.com>
2017-10-05 03:04:57 -0400

    Fix module definitions for timm

--- src/types/timm.d.ts
+++ src/types/timm.d.ts
@@ -1,7 +1,7 @@
-declare module 'timm'
-declare type Key = number | string
-declare type ArrayOrObject = any[] | object
+declare module 'timm' {
+  type Key = number | string
+  type ArrayOrObject = any[] | object
 
-declare function set (arr: any[], key: number, val: any): any[]
-declare function set (obj: object, key: string, val: any): object
-declare function omit (obj: object, attrs: string|string[]): object
+  function set<T extends {}> (obj: T, key: (string|number), val: any): T
+  function omit<T extends {}> (obj: T, attrs: string|string[]): T
+}
